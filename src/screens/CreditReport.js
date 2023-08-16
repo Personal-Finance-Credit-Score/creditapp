7bc3a0b596443f220cda341832f4dca93c4794de
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-08-15 22:56:17 -0500

    built out a majority of the screen

--- src/screens/CreditReport.js
+++ src/screens/CreditReport.js
@@ -1,13 +1,23 @@
-import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView, TextInput, Button } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView, TextInput, Button, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const CreditReport = (props) => {
   const navigation = useNavigation();
 
+  const [showAuto, setShowAuto] = useState("display: none")
+  const [showCC, setShowCC] = useState("display: none")
+  const [showHomeLoans, setShowHomeLoans] = useState("none")
+  const [showStudent, setShowStudent] = useState("none")
+
+
+  useEffect(() => {
+
+  }, [showCC])
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      
+      <ScrollView>
       {/* container for the page */}
       <View style={styles.container}>
 
@@ -21,13 +31,69 @@ const CreditReport = (props) => {
         {/* will replace with maybe drop down picker 
         https://www.npmjs.com/package/@react-native-picker/picker */}
         <View style={styles.dropDown}><Text>This is placeholder for now</Text></View>
-        <Text>Personal Information</Text>
-        <View></View>
+        <Text style={{ fontWeight: "bold", fontSize: 15 }}>Personal Information</Text>
+        <View style={styles.divider}></View>
+        <Text>Names Reported</Text>
+        <Text style={{ color: '#00000050'}}>Lorem Epsum</Text>
+        <Text>Employment Info</Text>
+        <Text>Addresses Reported</Text>
+        <Text style={{ fontWeight: 'bold'}}>Accounts</Text>
 
+        <View>
+            {/* section 1 */}
+            <TouchableOpacity 
+                style={styles.debtViewer}
+                onPress={()=> {
+                    setShowAuto()
+                }}>
+                <Text style={styles.debtText}>Auto Loans</Text>
+            </TouchableOpacity>
+            <View style={styles.debtShow}>
+                <Text>Tesla Motors Credit</Text>
+                <Text>$3,000.00</Text>
+                <Text>Reported: August 14, 2023</Text>
+            </View>
 
+            {/* section 2 */}
+            <TouchableOpacity style={styles.debtViewer} onPress={() => {
+                setShowAuto('display: flex')
+                console.log('presseddd')
+            }}>
+                <Text style={styles.debtText}>Credit Cards</Text>
+            </TouchableOpacity>
+            <View style={styles.textDebtShow}>
+                    <Text>Tesla Motors Credit</Text>
+                    <Text>$3,000.00</Text>    
+                </View>
+                <Text style={{marginLeft: 20}}>Reported: August 14, 2023</Text>
 
+            {/* section 3 */}
+            <TouchableOpacity style={styles.debtViewer}>
+                <Text style={styles.debtText}>Home Loans</Text>
+            </TouchableOpacity>
+            <View >
+                <View style={styles.textDebtShow}>
+                    <Text>Tesla Motors Credit</Text>
+                    <Text>$3,000.00</Text>    
+                </View>
+                <Text style={{marginLeft: 20}}>Reported: August 14, 2023</Text>
+            </View>
 
+            {/* section 4 */}
+            <TouchableOpacity style={styles.debtViewer}>
+                <Text style={styles.debtText}>Student Loans</Text>
+            </TouchableOpacity>
+            <View style={styles.debtShow}>
+                <View style={styles.textDebtShow}>
+                    <Text>Tesla Motors Credit</Text>
+                    <Text>$3,000.00</Text>    
+                </View>
+                <Text style={{marginLeft: 20}}>Reported: August 14, 2023</Text>
+            </View>
+            
+        </View>
       </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -36,6 +102,7 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flex: 1,
+    height: 1200,
     backgroundColor: '#64B4A1',
     alignItems: 'center',
     textAlign: 'center',
@@ -57,6 +124,37 @@ const styles = StyleSheet.create({
   },
   dateText: {
     marginTop: 20,
+  },
+  divider: {
+    margin: 10,
+    height: 1,
+    width: 300,
+    backgroundColor: 'black',
+  },
+  debtViewer: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    width: 400,
+    borderWidth: 2,
+    borderColor: "gray",
+    backgroundColor: 'white',
+  }, 
+  debtText: {
+    textAlign: 'center',
+  },
+  debtShow: {
+    display: 'flex',
+    paddingTop: 20,
+    paddingBottom: 20,
+    width: 396,
+    backgroundColor: '#D9D9D9'
+  },
+  textDebtShow: {
+    display: "flex", 
+    justifyContent: 'space-between', 
+    flexDirection: "row",
+    marginLeft: 20,
+    marginRight: 20,
   }
 
 });
