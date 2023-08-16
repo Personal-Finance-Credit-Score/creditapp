ac34dcdba5c4a1f30e2ffaa29f587a6aeeb8f832
ryanqle <ryan.q.le@gmail.com>
2023-08-16 14:14:25 -0500

    Intake form styling

--- src/screens/IntakeForm.js
+++ src/screens/IntakeForm.js
@@ -1,6 +1,6 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { SafeAreaView, TextInput, Button } from 'react-native';
+import { SafeAreaView, TextInput, Button, ScrollView } from 'react-native';
 import React, { useState } from 'react';
 
 const Login = (props) => {
@@ -29,84 +29,95 @@ const Login = (props) => {
 
   return (
     <SafeAreaView>
+      <ScrollView>
       <View className="pt-10 flex flex-1 mx-auto min-w-screen min-h-screen items-right text-center pt-30">
         <Text className="text-3xl font-bold">Enter your information</Text>
+        <Text></Text>
         <Text className="text-2xl font-semibold">This information will be used to create your account.</Text>
-        <Text className="text-lg font-bold">Your information is securely encrypted.</Text>
-        <Text className="text-lg font-bold">We’ll never sell your personal info.</Text>
-        <Text className="text-lg font-bold">Your credit won’t be impacted</Text>
-        <Text className="text-lg font-medium">First Name</Text>
+        <Text></Text>
+        <Text className="text-base font-bold">Your information is securely encrypted.</Text>
+        <Text className="text-base font-bold">We’ll never sell your personal info.</Text>
+        <Text className="text-base font-bold">Your credit won’t be impacted</Text>
+        <Text></Text>
+        <Text className="text-base font-medium pl-2">First Name</Text>
         <TextInput
-          className="relative rounded-sm border border-gray-300 bg-white w-80 h-10 indent-2.5"
+          className="relative rounded-sm border border-gray-300 bg-white w-80 h-10 pl-2.5 mb-2"
           placeholder="First Name"
           value={firstName}
           onChangeText={text => setFirstName(text)}
         />
-        <Text className="text-lg font-medium">Last Name</Text>
+        <Text className="text-base font-medium pl-2">Last Name</Text>
         <TextInput
-        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10"
+        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10 pl-2.5 mb-2"
           placeholder="Last Name"
           value={lastName}
           onChangeText={text => setLastName(text)}
         />
-        <Text className="text-lg font-medium">Date Of Birth</Text>
+        <Text className="text-base font-medium pl-2">Date Of Birth</Text>
         <TextInput
-        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10"
-          placeholder="Date of Birth"
+        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10 pl-2.5 mb-2"
+          placeholder="MM/DD/YYYY"
           value={dob}
           onChangeText={text => setDob(text)}
         />
-        <Text className="text-lg font-medium">Current Address</Text>
+        <Text className="text-base font-medium pl-2">Current Address</Text>
         <TextInput
-        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10"
+        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10 pl-2.5 mb-2"
           placeholder="Street"
           value={street}
           onChangeText={text => setStreet(text)}
         />
         <TextInput
-        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10"
+        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10 pl-2.5 mb-2"
           placeholder="City"
           value={city}
           onChangeText={text => setCity(text)}
         />
         <View className="flex flex-row space-x-4">
         <TextInput
-        className="relative rounded-sm border border-gray-300 bg-white w-40 h-10"
+        className="relative rounded-sm border border-gray-300 bg-white w-40 h-10 pl-2.5 mb-2"
           placeholder="State"
           value={state}
           onChangeText={text => setState(text)}
         />
         <TextInput
-        className="relative rounded-sm border border-gray-300 bg-white w-36 h-10"
+        className="relative rounded-sm border border-gray-300 bg-white w-36 h-10 pl-2.5 mb-2"
           placeholder="Zip"
           value={zip}
           onChangeText={text => setZip(text)}
         />
         </View>
-        <Text className="text-lg font-medium">Phone</Text>
+        <Text className="text-base font-medium pl-2">Phone</Text>
         <TextInput
-        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10"
+        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10 pl-2.5 mb-2"
           placeholder="Phone"
           value={phone}
           onChangeText={text => setPhone(text)}
         />
-        <Text className="text-lg font-medium">Social Security Number (SSN)</Text>
+        <Text className="text-base font-medium pl-2">Social Security Number (SSN)</Text>
         <TextInput
-        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10"
+        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10 pl-2.5 mb-2"
           placeholder="Last 4 of SSN"
           value={ssn}
           onChangeText={text => setSsn(text)}
         />
-        <Text className="text-lg font-medium">Income</Text>
+        <Text className="text-base font-medium pl-2">Income</Text>
         <TextInput
-        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10"
+        className="relative rounded-sm border border-gray-300 bg-white w-80 h-10 pl-2.5 mb-2"
           placeholder="Income"
           value={income}
           onChangeText={text => setIncome(text)}
         />
-
-        <Button title="Submit" onPress={handleFormSubmit} />
+        <TouchableOpacity
+        className="relative rounded-sm border bg-themeNavyBlue w-80 h-10 pl-2.5 mb-2"
+        title="Continue"
+        onPress={handleFormSubmit} >
+          <Text
+          className="text-white text-center align-middle"
+          >Continue</Text>
+        </TouchableOpacity>
       </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
