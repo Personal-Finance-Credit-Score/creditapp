c2aee5ed5cb44bfb9439ee42bf4953679a738d21
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-08-17 15:19:35 -0500

    finished horizontal nav bar, will build out other screens now

--- src/screens/CreditFactors/PaymentHistory.js
+++ src/screens/CreditFactors/PaymentHistory.js
@@ -13,7 +13,7 @@ import React, { useState, useEffect } from "react";
 import Avatar from "../../../assets/Avatar.png";
 import ArrowLeft from "../../../assets/arrowleft.png";
 import DownArrow from "../../../assets/downarrow.png";
-import PayHistory from '../../../assets/paymentHistory.png'
+import PayHistory from "../../../assets/paymentHistory.png";
 
 const PaymentHistory = (props) => {
   const navigation = useNavigation();
@@ -43,66 +43,129 @@ const PaymentHistory = (props) => {
         </View>
         <View className="shadow shadow-black "></View>
       </View>
-      
+
+      {/* horizontal nav bar */}
+      <ScrollView
+        horizontal
+        className="h-28"
+        style={{ android: { scrollbars: "none" } }}
+        showsHorizontalScrollIndicator={false}
+      >
+        <View className="bg-themeGreen flex-row justify-between p-3 w-[500px] mx-3">
+          <TouchableOpacity
+            className="w-20 bg-themeDarkGreen justify-center"
+            onPress={() => {
+              navigation.navigate("PaymentHistory");
+            }}
+          >
+            <Text className="text-themeWhite text-center">Payment History</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            className="w-20 justify-center"
+            onPress={() => {
+              navigation.navigate("PaymentHistory");
+            }}
+          >
+            <Text className="text-themeWhite text-center">Credit Card Use</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            className="w-20 justify-center"
+            onPress={() => {
+              navigation.navigate("PaymentHistory");
+            }}
+          >
+            <Text className="text-themeWhite text-center">Derogatory Marks</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            className="w-20 justify-center"
+            onPress={() => {
+              navigation.navigate("PaymentHistory");
+            }}
+          >
+            <Text className="text-themeWhite text-center">Total Account & Credit Age</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            className="w-20 justify-center"
+            onPress={() => {
+              navigation.navigate("PaymentHistory");
+            }}
+          >
+            <Text className="text-themeWhite text-center">Hard Inquires</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+
       {/* TODO: Need another navbar for each credit factor */}
 
       <ScrollView>
         {/* container for the page */}
         <View className="flex flex-1 w-screen px-8 max-w-md bg-testBgGreen pt-30 mb-32">
-          <View className='w-full'>
+          <View className="w-full">
             <Text className="text-3xl text-themeNavyBlue text-left mt-3">
-                Payment History
+              Payment History
             </Text>
           </View>
           <View className="flex-row justify-around mt-5">
-            <Image source={PayHistory}/>
+            <Image source={PayHistory} />
             <View className="flex flex-col justify-end mb-3">
-                <Text className="text-themeNavyBlue my-2">Percentage of score</Text>
-                <Text className="text-themeNavyBlue text-3xl">35%</Text>
+              <Text className="text-themeNavyBlue my-2">
+                Percentage of score
+              </Text>
+              <Text className="text-themeNavyBlue text-3xl">35%</Text>
             </View>
           </View>
 
-            <View className="mt-5">
-                <Text className="text-themeNavyBlue">How you’ve paid your bills in the past - whether you’ve paid on time or late, or missed payments - is a very important category of credit data for the FICO score. The more severe, recent and frequent the late payment information, the greater the impact on a FICO score.</Text>
-            </View>
-            <View className="items-center text-center bg-themeGreen py-3 mt-5 rounded">
-                <Text className="text-themeWhite">Your Payment History rating is</Text>
-                <Text className="text-themeWhite text-lg bg-themeDarkGreen p-3 mt-2">Exceptional</Text>
-            </View>
+          <View className="mt-5">
+            <Text className="text-themeNavyBlue">
+              How you’ve paid your bills in the past - whether you’ve paid on
+              time or late, or missed payments - is a very important category of
+              credit data for the FICO score. The more severe, recent and
+              frequent the late payment information, the greater the impact on a
+              FICO score.
+            </Text>
+          </View>
+          <View className="items-center text-center bg-themeGreen py-3 mt-5 rounded">
+            <Text className="text-themeWhite">
+              Your Payment History rating is
+            </Text>
+            <Text className="text-themeWhite text-lg bg-themeDarkGreen p-3 mt-2">
+              Exceptional
+            </Text>
+          </View>
 
-          <Text className="text-xl ml-2 text-themeNavyBlue mt-10 mb-3">Your Personal Data Points:</Text>
-
+          <Text className="text-xl ml-2 text-themeNavyBlue mt-10 mb-3">
+            Your Personal Data Points:
+          </Text>
 
           {/* Accounts Section */}
           <View className="items-center">
             {/* section 1 */}
-            <View
-              className="py-5 w-[350px] rounded px-5 flex-row bg-themeGreen justify-between">
+            <View className="py-5 w-[350px] rounded px-5 flex-row bg-themeGreen justify-between">
               <Text className="text-themeWhite">Collections</Text>
               <Text className="text-themeWhite">0</Text>
             </View>
 
             {/* section 2 */}
-            <View
-              className="py-5 m-1 w-[350px] px-5 flex-row rounded bg-themeGreen justify-between">
+            <View className="py-5 m-1 w-[350px] px-5 flex-row rounded bg-themeGreen justify-between">
               <Text className="text-themeWhite">Late Payment 30+ Days</Text>
               <Text className="text-themeWhite">0</Text>
             </View>
 
             {/* section 3 */}
-            <View
-              className="py-5 w-[350px] px-5 flex-row justify-between rounded bg-themeGreen">
+            <View className="py-5 w-[350px] px-5 flex-row justify-between rounded bg-themeGreen">
               <Text className="text-themeWhite">Collections</Text>
               <Text className="text-themeWhite">0</Text>
             </View>
 
             {/* section 4 */}
-            <View
-              className="py-5 m-1 w-[350px] px-5 flex-row justify-between rounded bg-themeGreen">
+            <View className="py-5 m-1 w-[350px] px-5 flex-row justify-between rounded bg-themeGreen">
               <Text className="text-themeWhite">Student Loans</Text>
               <Text className="text-themeWhite">0</Text>
             </View>
-            
           </View>
         </View>
       </ScrollView>
