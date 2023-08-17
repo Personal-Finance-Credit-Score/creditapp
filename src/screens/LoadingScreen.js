b80943de0252dcdfba4dd3a84d99b0e47f610cb3
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-08-17 08:35:25 -0500

    built out loading screen, will need animation though

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -2,16 +2,17 @@ import { View, Text, TouchableOpacity, Image} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView, TextInput, Button } from 'react-native';
 import React, { useState } from 'react';
-// import RiseLogo from '../../assets/riseLogo.png'
+import RiseLogo from '../../assets/riseLogo.png'
 
 const Login = (props) => {
   const navigation = useNavigation();
 
+  // TODO: Need loading animation???
+
   return (
-    <SafeAreaView >
-      <View >
-        <Text>Loading</Text>
-        {/* <Image className="w-342 h-349" source={RiseLogo}/> */}
+    <SafeAreaView className="flex-1 min-h-screen min-w-screen justify-center bg-themeLightBlue">
+      <View className="items-center">
+        <Image className="w-342 h-349" source={RiseLogo}/>
       </View>
     </SafeAreaView>
   );
