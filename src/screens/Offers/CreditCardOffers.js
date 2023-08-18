add3f8a2be12e041d62fbae1280149680b76cb79
bcstidd <bc.stidd@gmail.com>
2023-08-17 20:40:31 -0700

    Logo added top nav and card offer pg complete

--- src/screens/Offers/CreditCardOffers.js
+++ src/screens/Offers/CreditCardOffers.js
@@ -12,14 +12,15 @@ import {
   import React, { useState, useEffect } from "react";
   import Avatar from "../../../assets/Avatar.png";
   import ArrowLeft from "../../../assets/arrowleft.png";
-
+  import Logo from "../../../assets/riseLogo.png";
+  
   const CreditCardOffers = (props) => {
     const navigation = useNavigation();
     return (
       <SafeAreaView className="flex-1 min-w-screen min-h-screen bg-themeLightBlue">
         {/* Navbar */}
         <View className="h-1/8 pt-10 mb-1">
-          <View className="flex-row mt-6 mb-5 justify-between ml-4 ">
+          <View className="flex-row mt-6 mb-5 justify-between items-center ml-4 ">
             <TouchableOpacity
               className="mt-2"
               onPress={() => navigation.goBack()}
@@ -27,16 +28,12 @@ import {
               accessibilityLabel="Back"
               accessibilityHint="Navigates to the previous screen"
             >
-              <Image className="" source={ArrowLeft} />
+              <Image className="h-[37px] w-[37px]" source={Logo} />
             </TouchableOpacity>
             <Image className="mr-4" source={Avatar} />
           </View>
           <View className="shadow shadow-black "></View>
         </View>
-  
-        {/* horizontal nav bar */}
-       
-          
 
         <ScrollView>
           {/* container for the page */}
@@ -60,7 +57,7 @@ import {
                 </TouchableOpacity>
 
                 <TouchableOpacity
-                  className="w-1/2 h-[50px] justify-center rounded-t-xl border-2 bg-themeLightBlue"
+                  className="w-1/2 h-[50px] justify-center rounded-t-xl border-2 border-themeClearGray bg-themeLightBlue"
                   onPress={() => {
                     navigation.navigate("LoanOffers");
                   }}
@@ -73,32 +70,95 @@ import {
               <View className="h-full">
                 
                 {/* first card offer */}
-                <View className="p-2 border border-themeClearGray bg-themeWhite">
+                <View className="p-2 mb-2 border border-themeClearGray bg-themeWhite">
                   <View className="bg-themePlaceholder w-32 h-24 "></View>
-                  <Text className="font-bold text-themeNavyBlue mt-2 text-[17px]">Credit Card Name</Text>
+                  <Text className="font-bold text-themeNavyBlue mt-2 text-[17px]">Wells Fargo</Text>
                   <View className="flex-1 flex-row mt-[25px] space-x-3">
-                    <Image className="self-start ml-[10px]"source={Ava} />
+                    <Image className="self-start ml-[9px]"source={Ava} />
                   <Text className="text-[17px] mr-[20px]">Your chance of approval is <Text className="font-bold text-themeNavyBlue self-center">good</Text></Text></View>
-                </View>
-                <View className="ml-5 mt-2"><Text>Reward rate: 2x - 5x per dollar</Text></View>
-                <View className="ml-5 mt-2"><Text>Annual fee: $95</Text></View>
-                <View className="ml-5 mt-2"><Text>Welcome bonus: 75,000 miles</Text></View>                    
+                <View className="ml-5 mt-2"><Text>Reward rate: 2x - 3x per dollar</Text></View>
+                <View className="ml-5 mt-2"><Text>Annual fee: $5</Text></View>
+                <View className="ml-5 mt-2"><Text>Welcome bonus: 5,000 miles</Text></View>   
+                
                 <TouchableOpacity
                   className="bg-themeNavyBlue mt-5 w-100 h-[50px] justify-center border-2 "
                   onPress={() => {
                       navigation.navigate("Homescreen");
                     }}
                   >  
-                  <Text className="text-themeWhite text-center p-1">See more</Text>
+                  <Text className="text-themeWhite text-center p-1">View more</Text>
+                </TouchableOpacity> 
+                
+                <TouchableOpacity 
+                  className="bg-themeWhite mt-5 w-100 h-[50px] justify-center "
+                  onPress={() => {
+                      navigation.navigate("Homescreen");
+                    }}
+                  >
+                    <Text className="text-themeNavyBlue text-center p-1">See details and rates</Text>
+                </TouchableOpacity>
+                </View>
+                
+                {/* second card offer */}
+                <View className="p-2 border border-themeClearGray mb-2 bg-themeWhite">
+                  <View className="bg-themePlaceholder w-32 h-24 "></View>
+                  <Text className="font-bold text-themeNavyBlue mt-2 text-[17px]">Chase Sapphire</Text>
+                  <View className="flex-1 flex-row mt-[25px] space-x-3">
+                    <Image className="self-start ml-1"source={Ava} />
+                  <Text className="text-[17px] mr-[20px]">Your chance of approval is <Text className="font-bold text-themeGreen self-center">high</Text></Text></View>
+                <View className="ml-5 mt-2"><Text>Reward rate: 2x - 5x per dollar</Text></View>
+                <View className="ml-5 mt-2"><Text>Annual fee: $95</Text></View>
+                <View className="ml-5 mt-2"><Text>Welcome bonus: 75,000 miles</Text></View> 
+                <TouchableOpacity
+                  className="bg-themeNavyBlue mt-5 w-100 h-[50px] justify-center border-2 "
+                  onPress={() => {
+                      navigation.navigate("Homescreen");
+                    }}
+                  >  
+                  <Text className="text-themeWhite text-center p-1">View more</Text>
+                </TouchableOpacity>
+                
+                <TouchableOpacity 
+                  className="bg-themeWhite mt-5 w-100 h-[50px] justify-center "
+                  onPress={() => {
+                      navigation.navigate("Homescreen");
+                    }}
+                  >
+                    <Text className="text-themeNavyBlue text-center p-1">See details and rates</Text>
                 </TouchableOpacity>
 
                 </View>
-
-
-
-
+                {/* third card offer */}
+                <View className="p-2 mb-2 border border-themeClearGray bg-themeWhite">
+                  <View className="bg-themePlaceholder w-32 h-24 "></View>
+                  <Text className="font-bold text-themeNavyBlue mt-2 text-[17px]">Capital One</Text>
+                  <View className="flex-1 flex-row mt-[25px] space-x-3">
+                    <Image className="self-start ml-1"source={Ava} />
+                  <Text className="text-[17px] mr-[20px]">Your chance of approval is <Text className="font-bold text-orange-400 self-center">fair</Text></Text></View>
+                <View className="ml-5 mt-2"><Text>Reward rate: 4x per dollar</Text></View>
+                <View className="ml-5 mt-2"><Text>Annual fee: $200</Text></View>
+                <View className="ml-5 mt-2"><Text>Welcome bonus: 125,000 miles</Text></View> 
+                <TouchableOpacity
+                  className="bg-themeNavyBlue mt-5 w-100 h-[50px] justify-center border-2 "
+                  onPress={() => {
+                      navigation.navigate("Homescreen");
+                    }}
+                  >  
+                  <Text className="text-themeWhite text-center p-1">View more</Text>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                  className="bg-themeWhite mt-5 w-100 h-[50px] justify-center "
+                  onPress={() => {
+                      navigation.navigate("Homescreen");
+                    }}
+                  >
+                    <Text className="text-themeNavyBlue text-center p-1">See details and rates</Text>
+                </TouchableOpacity>
+                
+                </View>
+              </View>
+             </View>
             </View>
-          </View>
             
         </ScrollView>
       </SafeAreaView>
