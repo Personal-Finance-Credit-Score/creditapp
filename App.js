ebc00f78d61ab0300a11e43173001d4d1bec33f9
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-08-17 20:05:28 -0500

    added line chart package, cleaned up app.js

--- App.js
+++ App.js
@@ -1,66 +1,91 @@
 // In App.js in a new project
 
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import Homescreen from './src/screens/Homescreen';
-import Login from './src/screens/Login';
-import SignUpScreen from './src/screens/SignupScreen';
-import IntakeForm from './src/screens/IntakeForm';
-import ConfirmationScreen from './src/screens/ConfirmationScreen';
-import LoadingScreen from './src/screens/LoadingScreen';
-import VerificationScreen from './src/screens/VerificationScreen';
-import CreditReport from './src/screens/CreditReport';
-import ComingSoonScreen from './src/screens/ComingSoonScreen';
-import PaymentHistory from './src/screens/CreditFactors/PaymentHistory';
-import CreditCardUse from './src/screens/CreditFactors/CCUse';
-import DerogatoryMarks from './src/screens/CreditFactors/DerogatoryMarks';
-import CreditAge from './src/screens/CreditFactors/CreditAge';
-import HardInquiries from './src/screens/CreditFactors/HardInquiries';
+import * as React from "react";
+import { View, Text, Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Homescreen from "./src/screens/Homescreen";
+import Login from "./src/screens/Login";
+import SignUpScreen from "./src/screens/SignupScreen";
+import IntakeForm from "./src/screens/IntakeForm";
+import ConfirmationScreen from "./src/screens/ConfirmationScreen";
+import LoadingScreen from "./src/screens/LoadingScreen";
+import VerificationScreen from "./src/screens/VerificationScreen";
+import CreditReport from "./src/screens/CreditReport";
+import ComingSoonScreen from "./src/screens/ComingSoonScreen";
+import PaymentHistory from "./src/screens/CreditFactors/PaymentHistory";
+import CreditCardUse from "./src/screens/CreditFactors/CCUse";
+import DerogatoryMarks from "./src/screens/CreditFactors/DerogatoryMarks";
+import CreditAge from "./src/screens/CreditFactors/CreditAge";
+import HardInquiries from "./src/screens/CreditFactors/HardInquiries";
+import CreditTab from "./assets/CreditTab.png";
 
-import { supabase } from './lib/supabase'
-import { useState, useEffect } from 'react'
-import Auth from './src/screens/Auth';
-
+import { supabase } from "./lib/supabase";
+import { useState, useEffect } from "react";
+import Auth from "./src/screens/Auth";
 
 const Tab = createMaterialBottomTabNavigator();
 
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Homescreen" title="Home" component={Homescreen} options={{tabBarLabel:"Home"}} />
+      <Tab.Screen
+        name="Homescreen"
+        title="Home"
+        component={Homescreen}
+        options={{
+          tabBarLabel: "Credit",
+
+          tabBarIcon: () => {
+            return <Image source={CreditTab} />;
+          },
+        }}
+      />
       <Tab.Screen name="Login" component={Login} />
-      <Tab.Screen name="SignUp" component={SignUpScreen} options={{tabBarLabel:"SignUp"}} />
-      <Tab.Screen name="IntakeForm" component={IntakeForm} options={{tabBarLabel:"Intake"}} />
-      <Tab.Screen name="VerificationScreen" component={VerificationScreen} options={{tabBarLabel:"Verify"}}/>
-      <Tab.Screen name="CreditReport" component={CreditReport} options={{tabBarLabel:"CR"}} />
-      <Tab.Screen name="PaymentHistory" component={PaymentHistory} options={{tabBarLabel:"Pay"}} />
-      <Tab.Screen name="CreditCardUse" component={CreditCardUse} options={{tabBarLabel:"CC"}} />
-      <Tab.Screen name="DerogatoryMarks" component={DerogatoryMarks} options={{tabBarLabel:"DM"}} />
-      <Tab.Screen name="CreditAge" component={CreditAge} options={{tabBarLabel:"CAge"}} />
-      <Tab.Screen name="HardInquiries" component={HardInquiries} options={{tabBarLabel:"HI"}} />
+      <Tab.Screen
+        name="SignUp"
+        component={SignUpScreen}
+        options={{ tabBarLabel: "SignUp" }}
+      />
+      <Tab.Screen
+        name="IntakeForm"
+        component={IntakeForm}
+        options={{ tabBarLabel: "Intake" }}
+      />
+      <Tab.Screen
+        name="VerificationScreen"
+        component={VerificationScreen}
+        options={{ tabBarLabel: "Verify" }}
+      />
+      <Tab.Screen
+        name="CreditReport"
+        component={CreditReport}
+        options={{ tabBarLabel: "CR" }}
+      />
+      <Tab.Screen
+        name="PaymentHistory"
+        component={PaymentHistory}
+        options={{ tabBarLabel: "Pay" }}
+      />
     </Tab.Navigator>
   );
 }
 
-
 function App() {
-  const [session,setSession] = useState(null);
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
-      setSession(session)
-    })
-  }, [])  
+      setSession(session);
+    });
+  }, []);
   return (
-   
     <NavigationContainer>
       {/* {!session ? <Auth/> : <MyTabs key={session.user.id} session={session} />} */}
-    <MyTabs />
-  </NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
 
